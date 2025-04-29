316973ddc76159b58ca12d8f60157430122aa9d5
bright-dwarf <brian@pc-mate.com.au>
2025-04-29 17:08:40 +1000

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,8 +1,8 @@
 greeting = input("Greeting: ").strip().lower()
 
 if greeting.startswith("hello"):
-    print("$0")
+    print("$100")
 elif greeting.startswith("h"):
     print("$20")
 else:
-    print("$100")
+    print("$0")
